017c3b1bccfd0c615e4d6faf770272b32b7fe355
Collin Makwala <collin.makwala@ogilvy.co.za>
2025-02-05 07:41:06 +0200

    add consume github api

---
+++ Collin-Makwala-186-consume-github-api-python/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="consume_github_api",
+    version="0.0.1",
+    description="A Github API consumer",
+    long_description="A python script to use the GitHub API. It retrieves pull requests from a specified repository within a given date range.",
+    author="Collin Makwala",
+    author_email="collinmakwala@gmail.com",
+    license="Open Source",
+    url="https://github.com/Umuzi-org/Collin-Makwala-186-consume-github-api-python.git",
+    packages=["consume_github_api"],
+    zip_safe=False,
+)
